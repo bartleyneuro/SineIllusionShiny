abd578e189d18b26169013a286a99a21ede97776
srvanderplas <srvanderplas@gmail.com>
2013-04-16 07:54:01 -0500

    Fixed spinner interactions, including outputUI() changes.

--- server.R
+++ server.R
@@ -6,81 +6,83 @@ library(scrapeR)
 library(shinyIncubator)
 
 source("./functions.R")
-
+source("./inputSpinner.R")
 #' Look at https://groups.google.com/forum/?fromgroups=#!topic/shiny-discuss/THb1Ql5E20s for shiny server user-level data collection
 
 shinyServer(function(input, output, clientData) {
   suppressMessages(library(ggplot2))
-  
-  userResults <- data.frame()
+
+  wopts <- seq(0.05, .8, .05)
+  trialsreq <- expand.grid(w=c(0, sample(wopts, 2)), type=c("x", "y"))[sample(1:6, 6),]
+  trialsextra <- expand.grid(w=wopts, type=c("x", "y"))[sample(1:(2*length(wopts)), 2*length(wopts)),]
+  trials <- rbind(trialsreq, trialsextra)
+  trials$w <- ceiling(trials$w/0.05)*.05
+  reqtrials <- 6
+  maxtrials <- nrow(trials)
   
   output$illusion <- renderPlot({
+    
     f <- function(x) 2*sin(x)
     fprime <- function(x) 2*cos(x)
     f2prime <- function(x) -2*sin(x)
+    if(length(input$weight)<1) w <- 0 else w <- as.numeric(input$weight)
+    
     dframe <- createSine(n=40+2, len=1, f, fprime, f2prime, a=-pi, b=pi)[c(2:(41)),]
-    
-    trials <- expand.grid(w=c(0, .5, 1), type=c("x", "y"))
-    
-    minor.axis.correction <- correctx(seq(-pi, pi, pi/8), fprime, w=input$weight)
-    dframeAdj <- cbind(adjx(dframe, fprime=fprime, w=input$weight), adj=paste("X corrected, weight =", input$weight))
-#     dframe$type <- "Control"
-#     dframeAdj$type <- "Adjusted"
-#     dframe.all <- rbind.fill(dframe, dframeAdj)
-#     dframe.all$type <- factor(dframe.all$type, levels=c("Control", "Adjusted"))
-    dframe.all <- dframeAdj
-    p1 <- qplot(x=xstart, xend=xend, y=ystart, yend=yend, geom="segment", data=dframe.all) + coord_equal(ratio=1) +
-      theme_stimuli() + xlim(c(-pi, pi)) # + facet_wrap(~type)
+    dframe.all <- dframe
+    q <- input$q %% maxtrials + 1
+    if(trials$type[q]=="x"){
+      dframeAdj <- cbind(adjx(dframe, fprime=fprime, w=w), adj=paste("X corrected, weight =", w))
+      dframe.all <- dframeAdj
+    } else {
+      dframeAdj <- cbind(adjLinear(dframe, f, fprime, f2prime, w=w), adj=paste("Y corrected, weight =", w))
+      dframe.all <- dframeAdj
+    }
     
     cd <- names(clientData)
     cd2 <- t(ldply(cd, function(i) clientData[[i]]))
     names(cd2) <- cd
-    cd2 <- data.frame(id=input$id, final=input$submit, weight=input$weight,
-                      ipid=input$ipid, md5=input$finger, time=Sys.time(), cd2)
-    userResults <- rbind.fill(userResults, cd2)
+    inames <- names(input)
+    ivals <- t(ldply(inames, function(i) input[[i]]))
+    names(ivals) <- inames
+    cd2 <- data.frame(cbind(cd2, ivals))
+    names(cd2) <- c(cd, inames)
+    cd2$time <- Sys.time()
+    
+    cd2 <- cd2[,order(names(cd2))]
+    
+    
+    p1 <- qplot(x=xstart, xend=xend, y=ystart, yend=yend, geom="segment", data=dframe.all) + coord_equal(ratio=1) +
+      theme_stimuli() + xlim(c(-pi, pi)) # + facet_wrap(~type)
+
+    
     print(p1)
   })
-  
-  write.csv(userResults, "results.csv", row.names=FALSE)
-  
-  output$ycorrect <- renderPlot({
-    f <- function(x) input$amp*sin(x)
-    fprime <- function(x) input$amp*cos(x)
-    f2prime <- function(x) -input$amp*sin(x)
-    dframe <- createSine(n=input$obs+2, len=input$ell, f, fprime, f2prime, a=-pi, b=pi)[c(2:(input$obs+1)),]
-    corr <- which(input$correct==c("none", "geom", "linear", "quad"))
-    rat <- input$ell/(2*input$amp+input$ell)
-    
-    dframeAdj <- cbind(rbind(dframe, dframe), with(dframe, rbind(data.frame(seg.ystart=y-ell/2, seg.yend=y+ell/2, type="Segment"), data.frame(seg.ystart=ell/2, seg.yend=-ell/2, type="Adjustment"))), adj="Original Data")
 
-    if(corr==4){
-      dframeAdj1 <- adjQuad(dframe, f, fprime, f2prime)
-      title = "Correction: Quadratic"
-    } else if(corr==3){
-      dframeAdj1 <- adjLinear(dframe, f, fprime, f2prime)
-      title = "Correction: Linear"
-    } else if(corr==2){
-      dframeAdj1 <- adjGeom(dframe, f, fprime, f2prime)
-      title = "Correction: Trigonometric"
-    } else {
-      dframeAdj1 <- adjNone(dframe, f, fprime, f2prime)
-      title = "Correction: None"
-    }
-    limits <- range(c(subset(dframeAdj1, type=="Adjustment")$seg.ystart, 
-                      subset(dframeAdj, type=="Adjustment")$seg.ystart, 
-                      subset(dframeAdj1, type=="Adjustment")$seg.yend, 
-                      subset(dframeAdj, type=="Adjustment")$seg.yend))
-    if(corr==1) { 
-      dplot <- dframeAdj
-    } else{
-      dplot <- rbind(dframeAdj, dframeAdj1)
-    }
-    p <- qplot(x=x, xend=x, y=seg.ystart, yend=seg.yend, geom="segment", data=dplot) +
-      theme_bw() + coord_equal(ratio=1) + facet_grid(type~adj, scales="free_y", space="free_y") +
-      scale_x_continuous(breaks=seq(-pi, pi, by=pi/2), 
-                         labels=c(expression(-pi), expression(paste(-pi, "/2")), 0, 
-                                  expression(paste(pi,"/2")), expression(pi))) +
-      xlab("x") + ylab("y")
-    print(p)
+  writeResults <- reactive({
+      
+#       if("results.csv"%in%list.files())  {
+#         userResults <- read.csv("results.csv", stringsAsFactors=FALSE)
+#         userResults <- rbind.fill(userResults, cd2)
+#       } else {
+#         userResults <- cd2
+#       }
+#           
+#       write.csv(userResults, "results.csv", row.names=FALSE)
+      write.csv(cd2, "results.csv", row.names=FALSE, append=TRUE)
+    })
+  
+  output$testtext <- renderText({
+    paste(sapply(names(input), function(i) paste(i, "=", input[[i]], sep=" ")), collapse="\n")
   })
+  
+  output$questionCounter <- renderText({
+    if(input$q>=reqtrials) paste("Question ", input$q+1, " (", reqtrials," required).", sep="") else
+      paste("Question ", input$q+1 , " of ", reqtrials," required.\n", reqtrials - 1 - input$q, " remaining.", sep="")
+  })
+  
+  output$weightControl <- renderUI({
+    q <- input$q %% maxtrials + 1
+    inputSpinner("weight", value=trials$w[q]) 
+  })
+
 })
